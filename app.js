7e8de634399ac068861abee5174bd97dcbccbb0b
Ho Ngoc Dinh <d001595@cb.local>
2019-07-22 14:12:37 +0700

    Update env

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const exphbs = require('express-handlebars');
 
-const publicPath = path.join(__dirname, '/../public');
+const publicPath = path.join(__dirname, '/public');
 const port = process.env.PORT || 3000
 let app = express();
 let server = http.createServer(app);
