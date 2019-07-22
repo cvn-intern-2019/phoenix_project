d4cb40fb64ab2d3e08f163113f60c4e03dfb6395
Nguyen Quoc Huy <h001593@cb.local>
2019-07-22 16:54:57 +0700

    signin-signup

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const md5 = require('md5');
 
 const publicPath = path.join(__dirname, '/public');
 const port = process.env.PORT || 3000
@@ -10,7 +14,24 @@ let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
 
+let con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345678',
+    database: 'kahootdb'
+
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+
+
 app.use(express.static(publicPath));
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.engine('hbs', exphbs({
     defaultLayout: 'main.hbs',
     layoutsDir: 'views/_layouts',
@@ -20,8 +41,28 @@ app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
     res.render('index');
-    // console.log("FUCK");
 });
+
+app.get('/signin' , (req,res) => {
+    res.render('signIn');
+});
+
+app.get('/signup' , (req,res) => {
+    res.render('signUp');
+});
+
+app.post('/signup' , (req,res) => {
+    
+    let password = md5(req.body.password);
+    let sql = `INSERT INTO users (user_username,user_password,user_email) VALUES ('${req.body.username}','${password}','${req.body.email}')`;
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        res.render('index');
+    });   
+});
+
+
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
