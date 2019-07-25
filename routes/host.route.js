17a26e8dd8bced342ba9d1b4e487018a85601674
Ho Ngoc Dinh <d001595@cb.local>
2019-07-25 15:50:28 +0700

    Show questions list

--- routes/host.route.js
+++ routes/host.route.js
@@ -1,32 +1,37 @@
 const authMiddleware = require('../middlewares/auth.middleware');
 
-module.exports = function(app,passport) {
+module.exports = function(app, passport) {
     app.route('/host/signin')
-    .get((req,res) => {
-        res.render('signIn' , {signinuser : req.flash('signinuser'),signinpwd : req.flash('signinpwd')});
-    })
-    .post(passport.authenticate('local-signin' , {
-        successRedirect:'/host/profile',
-        failureRedirect:'/host/signin',
-        failureFlash: true
-    }))
-
-    app.route('/host/signup')
-        .get((req,res) => {
-            res.render('signUp' , {signupMessage : req.flash('signupMessage')});
+        .get((req, res) => {
+            res.render('signIn', { signinuser: req.flash('signinuser'), signinpwd: req.flash('signinpwd') });
         })
-        .post(passport.authenticate('local-signup' , {
-            successRedirect:'/host/signin',
-            failureRedirect:'/host/signup',
+        .post(passport.authenticate('local-signin', {
+            successRedirect: '/host/profile',
+            failureRedirect: '/host/signin',
             failureFlash: true
         }))
 
-    app.get('/host/signout',authMiddleware.isSignIn,(req, res) => {
+    app.route('/host/signup')
+        .get((req, res) => {
+            res.render('signUp', { signupMessage: req.flash('signupMessage') });
+        })
+        .post(passport.authenticate('local-signup', {
+            successRedirect: '/host/signin',
+            failureRedirect: '/host/signup',
+            failureFlash: true
+        }))
+
+    app.get('/host/signout', authMiddleware.isSignIn, (req, res) => {
         req.logout();
         res.redirect('/');
     });
 
-    app.get('/host/profile',authMiddleware.isSignIn, (req,res) => {
-        res.render('profile',{user : req.user});
+    app.get('/host/profile', authMiddleware.isSignIn, (req, res) => {
+        res.render('profile', { user: req.user });
     })
+
+    app.route('/host/questions')
+        .get((req, res) => {
+            res.render('questions/list');
+        })
 };
