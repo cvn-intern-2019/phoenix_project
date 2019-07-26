4622f91f3e1fa9c47dc14df42851a0a4ab98351d
Truong Thanh Danh <d001588@cb.local>
2019-07-26 16:56:07 +0700

    add / edit questionset

--- models/questionset/questionset.model.js
+++ models/questionset/questionset.model.js
@@ -4,12 +4,4 @@ module.exports = {
     all: () => {
         return db.query('select * from questionsets');
     },
-
-    single: id => {
-        return db.query(`select * from questionsets where questionset_id = ${id}`);
-    },
-
-    add: user => {
-        return db.query(`INSERT INTO questionsets (questionset_title,questionset_description,questionset_image,questionset_state) VALUES ("${user.title}","${user.description}","${user.file}","${user.status}")`);
-    }
 };
--- routes/questionset/questionset.route.js
+++ routes/questionset/questionset.route.js
@@ -1,12 +1,12 @@
 var questionset = require('../../models/questionset/questionset.model');
-
 var db = require('../../utils/db');
+
+
 
 module.exports = function (app, test) {
     app.get('/questionset', (req, res) => {
         var p = questionset.all();
         p.then(rows => {
-            console.log(rows);
             res.render('questionsets/questionset', {
                 questionset: rows
             });
@@ -24,39 +24,67 @@ module.exports = function (app, test) {
             let questionset = req.body;
             test(req, res, err => {
                 if (err) {
-                    res.render('questionset/add_questionset'); 
+                    res.render('questionset/add_questionset');
                 } else {
                     var filename = "";
-                    if(req.file){
-                        filename=req.file.filename;
+                    if (req.file) {
+                        filename = req.file.filename;
                     }
                     var sql = `INSERT INTO questionsets (questionset_title,questionset_description,questionset_image,questionset_state) 
                     VALUES ("${questionset.title}","${questionset.description}","${filename}","${questionset.status}")`;
                     db.query(sql)
-                    .then(result => {
-                        res.redirect('/questionset'); 
-                    })
-                    .catch(err => {
-                        res.render('questionset/add_questionset'); 
-                    });
+                        .then(result => {
+                            res.redirect('/questionset');
+                        })
+                        .catch(err => {
+                            res.render('questionset/add_questionset');
+                        });
                 }
             })
         })
 
+    app.route('/questionset/:qs_id/edit')
+        .get((req, res) => {
+            let sql = `select * from questionsets where questionset_id = '${req.params.qs_id}'`;
+            db.query(sql)
+                .then(result => {
+                    let path = '/img/' + result[0].questionset_image;
+                    res.render('questionsets/edit_questionset', { questionset: result[0],path:path, csrfToken: req.csrfToken() });
+                })
+                .catch(err => {
+                    res.redirect('/questionset');
+                });
+        })
 
-    // app.get('/edit/:id', (req, res) => {
-    //     var id = req.params.id;
-    //     questionset.single(id).then(rows => {
-    //         if (rows.lenght > 0) {
-    //             res.render('questionsets/edit_questionset', {
-    //                 error: false,
-    //                 question_set: rows[0]
-    //             });
-    //         } else {
-    //             res.render('questionsets/edit_questionset', {
-    //                 error: true,
-    //             });
-    //         }
-    //     });
-    // })
-};
+        .post((req, res) => {
+            let questionset = req.body;
+            let path = './public/img/' + questionset.questionset_image;
+            test(req, res, err => {
+                if (err) {
+                    res.render('questionset/questionset');
+                } else {
+                    var filename = "";
+                    if (req.file) {
+                        filename = req.file.filename;
+                    }
+                    var sql = `UPDATE questionsets SET questionset_title = '${questionset.title}',questionset_description = '${questionset.description}',questionset_image = '${filename}',questionset_state = '${questionset.status}'  WHERE questionset_id = '${req.params.qs_id}';`;
+                    console.log(sql);
+                    db.query(sql)
+                        .then(result => {
+                            res.redirect('/questionset');
+                        })
+                        .catch(err => {
+                            res.render('questionset/add_questionset');
+                        });
+                }
+            })
+            // let sql = `UPDATE questionsets SET questionset_title = '${req.body.title}',questionset_description = '${req.body.description}',questionset_image = '${req.body.image}', questionset_state = '${req.body.status}'  WHERE questionset_id = '${req.params.qs_id}';`
+            // db.query(sql)
+            //     .then(result => {
+            //         res.redirect('/questionset');
+            //     })
+            //     .catch(err => {
+            //         console.log(err);
+            //     });
+        })
+}
