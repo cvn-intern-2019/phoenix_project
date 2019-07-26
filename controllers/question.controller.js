9599c1f291dc25c0221eaf13be957d6baec9a9e3
Nguyen Quoc Huy <h001593@cb.local>
2019-07-26 14:20:25 +0700

    update question controller

--- controllers/question.controller.js
+++ controllers/question.controller.js
@@ -32,5 +32,31 @@ module.exports = {
             .catch(err => {
                 console.log(err);
             });
+    },
+    findQuestion : (req,res) => {
+        let sql = `SELECT * FROM questions WHERE question_id = ${req.params.q_id} AND questionset_id = ${req.params.qs_id}`;
+        question_model.exec(sql)
+            .then(result => {
+                if(result.length == 0) {
+                    res.send('Question not found');
+                }
+
+                res.render('questions/update',{
+                    csrfToken: req.csrfToken(),
+                    question:result[0],
+                    user : req.user
+                })
+            })
+    },
+    editQuestion : (req,res) => {
+        let sql = `UPDATE questions SET question_content = '${req.body.content}',question_answer1 = '${req.body.answer1}',question_answer2 = '${req.body.answer2}',question_answer3 = '${req.body.answer3}',question_answer4 = '${req.body.answer4}',question_answercorrect = '${req.body.correctanswer}'  WHERE question_id = ${req.params.q_id};`
+        question_model.exec(sql)
+            .then(result => {
+                res.redirect(`/host/questionset/${req.params.qs_id}/question`);
+            })
+            .catch(err => {
+               console.log(err);
+
+            });
     }
 };
--- routes/question.route.js
+++ routes/question.route.js
@@ -1,10 +1,14 @@
 const authMiddleware = require('../middlewares/auth.middleware');
 const question_controller = require('../controllers/question.controller');
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.route('/host/questionset/:id/question')
-        .get(question_controller.showQuestionList);
+        .get(authMiddleware.isSignIn,question_controller.showQuestionList);
 
     app.route('/host/questionset/:qs_id/question/:q_id/delete')
-        .get(question_controller.deleteQuestion);
+        .get(authMiddleware.isSignIn,question_controller.deleteQuestion);
+
+    app.route('/host/questionset/:qs_id/question/:q_id/edit')
+        .get(authMiddleware.isSignIn, question_controller.findQuestion)
+        .post(question_controller.editQuestion);
 };
