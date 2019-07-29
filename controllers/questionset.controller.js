5f2b70d6c3247a31e5ba96239a86208b78ac1475
Truong Thanh Danh <d001588@cb.local>
2019-07-29 13:44:08 +0700

    test

--- controllers/questionset.controller.js
+++ controllers/questionset.controller.js
@@ -28,12 +28,9 @@ module.exports = {
                 console.log(err);
             })
     },
-
     addquestionset: (req, res) => {
         res.render('questionsets/add_questionset', { user: req.user, csrfToken: req.csrfToken()});
     },
-
-
     savequestionset: (req, res) => {
         let questionset = req.body;
         upload(req, res, err => {
@@ -52,8 +49,6 @@ module.exports = {
                         db.query(sql2)
                         .then(result => {
                             var sql = `INSERT INTO user_questionset (user_id, questionset_id) VALUES ("${req.user.user_id}","${result[0].questionset_id}")`;
-                            console.log(result[0].questionset_id);
-                            console.log(sql);
                             db.query(sql)
                             .then(result =>{
                                 res.redirect('/host/questionset');
@@ -73,7 +68,6 @@ module.exports = {
             }
         })
     },
-
     findquestionset: (req, res) => {
         let sql = `SELECT * FROM questionsets WHERE questionset_id = '${req.params.qs_id}'`;
         db.query(sql)
@@ -82,10 +76,9 @@ module.exports = {
                 res.render('questionsets/edit_questionset', { questionset: result[0], path: path, csrfToken: req.csrfToken() });
             })
             .catch(err => {
-                res.redirect('/questionset');
+                res.redirect('/host/questionset');
             });
     },
-
     editquestionset: (req, res) => {
         let img_delete_query = `SELECT questionset_image FROM questionsets WHERE questionset_id = ${req.params.qs_id}`;
         questionset_model.exec(img_delete_query)
@@ -122,12 +115,30 @@ module.exports = {
                 console.log(sql);
                 db.query(sql)
                     .then(result => {
-                        res.redirect('/questionset');
+                        res.redirect('/host/questionset');
                     })
                     .catch(err => {
                         res.render('questionsets/add_questionset');
                     });
             }
         })
-    }
+    },
+
+    
+    create_room: (req, res) => {
+        let sql = `SELECT questionsets.* FROM questionsets, user_questionset 
+        WHERE user_questionset.questionset_id = questionsets.questionset_id 
+        and questionsets.questionset_id = ${req.params.qs_id} and user_questionset.user_id = ${req.user.user_id}`;
+        db.query(sql)
+            .then(result => {
+                if(result[0]){
+                    res.render('waiting_room', { questionsets: result, csrfToken: req.csrfToken()});
+                }else{
+                    res.redirect('/host/questionset');
+                }
+            })
+            .catch(err => {
+                res.redirect('/host/questionset');
+            });
+    },
 };
