8f6cea49ee8131cbbb8f6d3783d2dcb69c94ee71
Nguyen Quoc Huy <h001593@cb.local>
2019-07-30 16:36:24 +0700

    finalStatUI

--- routes/player.route.js
+++ routes/player.route.js
@@ -1,7 +1,11 @@
 const authMiddleware = require('../middlewares/auth.middleware');
 
 module.exports = (app) => {
-    app.get('/player', authMiddleware.alreadySignin, (req, res) => {
+    app.get('/player', (req, res) => {
         res.render('player/home')
+    });
+
+    app.get('/player/final-stat' , (req,res) => {
+        res.render('player/final-stat');
     });
 }
